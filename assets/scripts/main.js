f1c24b62790ac6b4b0b5f35448ccb6f699282539
Muhammet Emin Aynacı <aynacimuhammetemin@gmail.com>
2022-02-23 20:36:54 +0300

    LazyLoad

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,37 +1,40 @@
-$(window).on("load",function(){
-    setTimeout(() => {
-    $(".loader-wrapper").fadeOut();
-    }, 1300);
+$(function () {
+  $('.lazy').Lazy();
+  $('.search_icon').click(function () {
+    $('#search').css("visibility", "visible");
+    $('.search_close').css("visibility", "visible");
+  });
 
-    $('.search_icon').click(function(){
-        $('#search').css("visibility","visible"); 
-        $('.search_close').css("visibility","visible");
-    });
+  $('.search_close').click(function () {
+    $('#search').css("visibility", "hidden");
+    $(this).css("visibility", "hidden");
+  });
 
-    $('.search_close').click(function(){
-        $('#search').css("visibility","hidden");
-        $(this).css("visibility","hidden");
-    });
+  $('.show_more').on("click", function () {
+    $(this).parent().find("div.summary").css("height", "auto");
+    $(this).css("display", "none");
+    $(this).parent().find("span.not_read").css("display", "block");
+  })
 
-    $('.show_more').on("click", function(){
-        $(this).parent().find("div.summary").css("height","auto");
-        $(this).css("display","none");
-        $(this).parent().find("span.not_read").css("display","block");
-      })
+  $('.not_read').on("click", function () {
+    $(this).parent().find("div.summary").css("height", "120px");
+    $(this).css("display", "none");
+    $(this).parent().find("span.show_more").css("display", "block");
+  })
 
-      $('.not_read').on("click", function(){
-        $(this).parent().find("div.summary").css("height", "120px");
-        $(this).css("display","none");
-        $(this).parent().find("span.show_more").css("display","block");
-     })
+  $('.detail_slider_photo').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  });
 
-     $('.detail_slider_photo').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-      });
-                    
+
 });
 
+$(window).on("load", function () {
+  setTimeout(() => {
+    $(".loader-wrapper").fadeOut();
+  }, 1300);
+});
