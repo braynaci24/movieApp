31191ddb62f09e3b1efcb3fa5cf8f7ae85d632a9
Muhammet Emin Aynacı <aynacimuhammetemin@gmail.com>
2022-02-24 15:21:53 +0300

    Get data & create detail

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,74 @@
+let pageUrl = window.location.search;
+let parameters = new URLSearchParams(pageUrl);
+let value = parameters.get('id');
+console.log(parameters, pageUrl, value);
+let loading = true;
 $(function () {
+
+  const apiKey = 'd5b08408';
+
+  function searchMovie() {
+    $('#search').keypress(function (e) {
+      let searchVal = $('#search').val();
+      var url = "http://www.omdbapi.com/?apikey=" + apiKey;
+      var key = e.which;
+      if (key == 13) {
+        $(this).val(" ")
+        $.ajax({
+          method: "GET",
+          url: url + "&s=" + searchVal,
+          success: function (data) {
+            createMovieList(data)
+          }
+        })
+      }
+    });
+  }
+
+  function createMovieList(movieData) {
+    $('.dont_title').remove();
+    for (let i = 0; i < movieData.Search.length; i++) {
+      if(movieData.Search[i].Poster == 'N/A'){
+        i++;
+      }
+      let movieHtml = `
+    <a href="/detail.html?id=${movieData.Search[i].imdbID}" class="movie_item_container">
+      <img src="${movieData.Search[i].Poster}" class="lazy main_moive_photo" alt="">
+      <span class="movie_year">${movieData.Search[i].Year}</span>
+      <h3 class="movie_title">${movieData.Search[i].Title}</h3>
+    </a>
+    `
+      $('#main').append(movieHtml);
+    }
+  }
+
+  function createMovieDetail(data) {
+    $('.detail_banner_top').attr("src", `${data.Poster}`)
+    $('.bottom_banner').attr("src", `${data.Poster}`)
+    $('.movie_photo_slider_item').attr("src", `${data.Poster}`)
+    $('.summary_paragraph').text(data.Plot);
+    $('.detail_rating_movie').text(data.imdbRating + '/10');
+    $('.movie_big_title').text(data.Title);
+    $('.movie_box_infos').text(data.Genre);
+    $('.blog_item_image').attr("src", `${data.Poster}`)
+    $('.blog_title').text(data.Title);
+  }
+
+  function getDetails(movieId) {
+    console.log(movieId)
+    $.ajax({
+      method: "GET",
+      url: 'http://www.omdbapi.com/?i=' + movieId + '&apikey=' + apiKey,
+      success: function (dataDetail) {
+        createMovieDetail(dataDetail)
+        loading = true;
+      }
+    })
+  }
+
+  getDetails(value);
+  searchMovie();
+
   $('.lazy').Lazy();
   $('.search_icon').click(function () {
     $('#search').css("visibility", "visible");
@@ -25,8 +95,6 @@ $(function () {
   $('.detail_slider_photo').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
     arrows: false,
   });
 
@@ -34,7 +102,11 @@ $(function () {
 });
 
 $(window).on("load", function () {
-  setTimeout(() => {
+  if(window.location.href.indexOf('/detail.html?id=tt') >= 0 && loading){
     $(".loader-wrapper").fadeOut();
-  }, 1300);
+  }else {
+    setTimeout(() => {
+      $(".loader-wrapper").fadeOut();
+    }, 1300);
+  }
 });
